d8fc8a33065f496c08dbfe114a7f922fc0b87ab5
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-16 20:16:13 -0300

    Arquivos aula 01 fundamentos

---
+++ modulo01-fundamentos/20221116-aula01/001.py
@@ -0,0 +1,40 @@
+
+# Esse é um comentário padrão do Python
+
+"""
+Isso é uma string multilinha
+
+Ela é utilizada para gerar documentação de funcões, métodos e classes no Python
+
+Também pode ser utilizada como comentário
+"""
+
+"""
+Executamos um arquivo python utilizando a seguinte sintaxe:
+
+python <nome_do_arquivo.py>
+
+"""
+
+# Imprime a mensagem "Hello, world!" no terminal.
+
+"""
+Não é obrigatório, porém é recomendável utilizarmos a condição 'if __name__ == '__main__': no arquivo principal da
+aplicação, indicando que ele é o entrypoint (ponto de partida) da aplicação.
+
+Os dois pontos ':' indicam o início de um novo bloco de código.
+
+ATENÇÃO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+*** Todo bloco de código deve ter o espaçamento correto com relação ao anterior ***
+
+ponto-e-vírgula somente é usado quando queremos ter mais de 1 statement (comando) por linha, mesmo assim não é
+recomendado o seu uso
+
+É muito importante seguir a identação correta dos blocos, a fim de evitar exceções do tipo IndentationError
+
+"""
+
+if __name__ == "__main__":
+    print("Hello, world!")
+
