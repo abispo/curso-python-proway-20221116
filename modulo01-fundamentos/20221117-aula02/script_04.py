4bddc43c36d3eec90ae7b888f17a8e39d338394d
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-18 18:54:54 -0300

    Arquivos aula 03 fundamentos

--- modulo01-fundamentos/20221117-aula02/script_04.py
+++ modulo01-fundamentos/20221117-aula02/script_04.py
@@ -41,3 +41,42 @@ if __name__ == "__main__":
     lista4 = ["Batata", "Tomate", "Alface", "Brócolis"]
     lista3.extend(lista4)
     print(lista3)
+
+    # 20221118
+    # Para excluir itens de uma lista, podemos utilizar 4 maneiras
+
+    # remove()  -> Remove itens de uma lista a partir do valor
+    lista3.remove("Alface")
+    print(lista3)
+
+    # pop()     -> Remove um item da lista a partir do seu índice, e retorna esse item
+    valor_removido = lista3.pop(2)
+    print(f"Valor removido: {valor_removido}")
+    print(lista3)
+
+    # del       -> del é uma palavra reservada que é utilizada para remover objetos da memória
+    # Utilizamos o índice da lista para excluir o item
+    del lista3[5]
+    print(lista3)
+
+    # clear()   -> Apaga todos os itens da lista
+    lista3.clear()
+    print(lista3)
+
+    print(f"{'-'*15} 'fatiando(slicing)' listas {'-'*15}")
+
+    lista1 = ["Brasil", "França", "Inglaterra", "Argentina", "Bélgica", "Alemanha"]
+    #           0           1           2           3           4           5
+    #           -6          -5          -4          -3          -2          -1
+
+    """
+    Fatiamos uma sequência (listas, tuplas, strings, etc) utilizando a seguinte sintaxe
+    sequencia[inicio:fim:passo]
+    inicio  -> Índice inicial de onde vamos pegar o "pedaço" da sequência. Ele é inclusivo.
+    fim     -> Índice final de onde vamos pegar o "pedaço" da sequência. Ele é exclusivo
+    passo   -> Se quantos em quantos itens vamos extrair esse "pedaço"
+    """
+
+    print(lista1[2:5])      # Pegando os itens 3 a 5 da sequência
+    print(lista1[-1])       # Retornando apenas o último item
+    print(lista1[2:])       # A partir do índice 2, retorna todos os itens restantes
