0a17be1376b25d63d82bf478cec16ae279d9fe81
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-22 19:54:35 -0300

    Arquivos aula 05 fundamentos

---
+++ modulo01-fundamentos/20221122-aula05/script_01.py
@@ -0,0 +1,51 @@
+"""
+Entrada e saída de arquivos txt
+I/O de arquivos (Input/Output)
+
+"""
+
+import os
+
+if __name__ == "__main__":
+
+    # Para abrir arquivos pelo python, utilizamos a função built-in open()
+    # Temos que passar como argumentos o arquivo que deve ser aberto e o modo de abertura
+    # Se o modo de abertura não for informado, o Python abrirá automaticamente esse arquivo
+    # como somente-leitura.
+    # A função open() retorna o objeto que representará esse arquivo.
+
+    # os.path.join() -> Ela concatena nomes de diretórios e nomes de arquivos, garantindo assim
+    # que o caminho até o arquivo esteja correto
+    # os.getcwd() retorna o caminho completo até o diretório de onde o script está sendo
+    # executado
+    arquivo = open(os.path.join(os.getcwd(), "log.txt"), encoding="utf-8")
+
+    # Podemos ler arquivos texto no python de 4 maneiras diferentes
+
+    # Usando o método read(n)
+    # n significa quantos caracteres serão lidos. Se não for passado, lê o arquivo inteiro
+    # read() retorna uma string com o conteúdo lido
+    print(arquivo.read())
+
+    # Método readline(n)
+    # Lê uma linha inteira do arquivo até a quantidade de caracteres informada
+    print(arquivo.readline())
+
+    # A linha acima não imprime nada, pois o cursor do arquivo está no final dele (pois chamamos)
+    # o método read(). Podemos verificar a posição atual do cursor utilizando o método
+    # tell
+    print(arquivo.tell())
+
+    # Se quisermos ler o arquivo novamente, precisamos "rebobinar" esse cursor até o início do
+    # arquivo (posição 0). Utilizamos o método seek() para isso
+    arquivo.seek(0)
+
+    print(arquivo.readline(10))
+    print(arquivo.readline())
+
+    # readlines()
+    # Lê as linhas do arquivo e retorna como itens de uma lista
+    print(arquivo.readlines(100))
+
+    # Sempre que abrimos um arquivo, devemos fechá-lo
+    arquivo.close()
