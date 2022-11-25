f60d95d9945ae412753c59d6a88d4a03fe81d19e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-25 18:45:21 -0300

    Arquivos aula 02 banco

--- modulo02-python-com-banco-de-dados/20221124-aula02/script_03.py
+++ modulo02-python-com-banco-de-dados/20221124-aula02/script_03.py
@@ -31,19 +31,29 @@ class PagamentoPorBoleto(Pagamento):
 class PagamentoPorCartaoDeCredito(Pagamento):
 
     def __init__(self, valor, numero_cartao, codigo_seg):
-        # super()
-        self._valor = valor
+        # A função built-in super() chama um método da superclasse.
+        # Nesse caso, estamos chamando o método construtor de Pagamento passando o parâmetro valor
+        super().__init__(valor)
         self._numero_cartao = numero_cartao
         self._codigo_seg = codigo_seg
 
+    # Esse método retorna um valor booleano aleatório.
+    # Python considera o valor 0 como False, e qualquer valor diferente de 0 como True
     def _tem_limite_disponivel(self):
         return bool(randint(0, 1))
 
     def pagar(self):
         if self._tem_limite_disponivel():
             print(f"O valor de {self._valor} foi pago com cartão de crédito.")
+        else:
+            print("Não há limite disponível")
 
 
 if __name__ == "__main__":
     pagamento_por_boleto = PagamentoPorBoleto(200)
     pagamento_por_boleto.pagar()
+
+    pagamento_por_cartao_de_credito = PagamentoPorCartaoDeCredito(
+        300, "8789234561789001", "001"
+    )
+    pagamento_por_cartao_de_credito.pagar()
