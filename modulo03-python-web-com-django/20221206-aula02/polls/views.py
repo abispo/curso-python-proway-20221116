943a64e152d17cc3e8d4ea084fba086d0d6a41f8
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-06 22:09:41 -0300

    Arquivos aula 02 python web

--- modulo03-python-web-com-django/20221206-aula02/polls/views.py
+++ modulo03-python-web-com-django/20221206-aula02/polls/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Question
+
+
 # function-based view
 def index(request):
-    return HttpResponse("Olá mundo! Página index do polls app.")
+    latest_question_list = Question.objects.order_by("-pub_date")
+
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Você está vendo os detalhes da pergunta {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Você está vendo os resultados da pergunta {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Você está votando na pergunta {question_id}.")
