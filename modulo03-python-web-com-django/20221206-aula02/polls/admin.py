c6c81193f56780654cbc362877b42548616d2637
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-06 21:02:12 -0300

    Arquivos aula 02 python web

--- modulo03-python-web-com-django/20221206-aula02/polls/admin.py
+++ modulo03-python-web-com-django/20221206-aula02/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
--- modulo03-python-web-com-django/20221206-aula02/polls/models.py
+++ modulo03-python-web-com-django/20221206-aula02/polls/models.py
@@ -6,6 +6,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Data de publicação')
 
+    def __str__(self):
+        return self.question_text
+
     class Meta:
         db_table = 'tb_questions'
 
@@ -15,6 +18,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
     class Meta:
         db_table = 'tb_choices'
