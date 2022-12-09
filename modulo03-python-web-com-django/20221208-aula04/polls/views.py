870e469d7de673834e922c077f572781079d96ca
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-08 21:11:28 -0300

    Arquivos aula 04 python web

--- modulo03-python-web-com-django/20221208-aula04/polls/views.py
+++ modulo03-python-web-com-django/20221208-aula04/polls/views.py
@@ -30,7 +30,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Você está vendo os resultados da pergunta {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
