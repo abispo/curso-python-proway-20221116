5ac1b3047a56b5cc76c252c50242a82c29e2b30c
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-13 21:53:17 -0300

    Arquivos aula 07 python web

--- modulo03-python-web-com-django/20221213-aula07/financas/urls.py
+++ modulo03-python-web-com-django/20221213-aula07/financas/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index
+from .views import index, transacoes_por_usuario
 
 app_name = "financas"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "<int:user_id>/transacoes",
+        transacoes_por_usuario,
+        name="transacoes_por_usuario"
+    )
 ]
--- modulo03-python-web-com-django/20221213-aula07/financas/views.py
+++ modulo03-python-web-com-django/20221213-aula07/financas/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from .models import Transacao
+
 
 def index(request):
-    return render(request, "financas/index.html")
+
+    num_transacoes = Transacao.objects.filter(
+        conta_debito__usuario=request.user
+    ).count()
+
+    context = {
+        "num_transacoes": num_transacoes
+    }
+
+    return render(
+        request, "financas/index.html", context
+    )
+
+
+def transacoes_por_usuario(request, user_id):
+
+    return render(request, "financas/transacoes_por_usuario.html")
