3de1532067ef323f19031cd41133e02712e2e7ea
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-19 18:41:21 -0300

    Arquivos aula 11 python web

---
+++ modulo03-python-web-com-django/20221219-aula11/financas/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+
+from .views import (
+    index,
+    transacoes_por_usuario,
+    contas_por_usuario,
+    nova_conta,
+    nova_transacao,
+    detalhe_conta
+)
+
+app_name = "financas"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "transacoes/",
+        transacoes_por_usuario,
+        name="transacoes_por_usuario"
+    ),
+    path(
+        "contas/",
+        contas_por_usuario,
+        name="contas_por_usuario"
+    ),
+    path(
+        "contas/nova/",
+        nova_conta,
+        name="nova_conta"
+    ),
+    path(
+        "transacoes/nova/",
+        nova_transacao,
+        name="nova_transacao"
+    ),
+    path(
+        "contas/<int:conta_id>/",
+        detalhe_conta,
+        name="detalhe_conta"
+    )
+]
---
+++ modulo03-python-web-com-django/20221219-aula11/financas/views.py
@@ -0,0 +1,153 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.db.models import Q
+
+from .models import Transacao, ContaFinanceira
+
+
+def index(request):
+
+    num_transacoes = Transacao.objects.filter(
+        conta_debito__usuario=request.user
+    ).count()
+
+    num_contas = ContaFinanceira.objects.filter(
+        usuario=request.user
+    ).count()
+
+    context = {
+        "num_transacoes": num_transacoes,
+        "num_contas": num_contas
+    }
+
+    return render(
+        request, "financas/index.html", context
+    )
+
+
+def transacoes_por_usuario(request):
+
+    todas_transacoes = Transacao.objects.filter(
+        conta_debito__usuario=request.user
+    )
+
+    context = {
+        "todas_transacoes": todas_transacoes
+    }
+
+    return render(
+        request,
+        "financas/transacoes_por_usuario.html",
+        context
+    )
+
+
+def contas_por_usuario(request):
+    todas_contas = ContaFinanceira.objects.filter(
+        usuario=request.user
+    )
+
+    context = {
+        "todas_contas": todas_contas
+    }
+
+    return render(
+        request,
+        "financas/contas_por_usuario.html",
+        context
+    )
+
+
+def nova_conta(request):
+
+    if request.method == "GET":
+        return render(request, "financas/nova_conta.html")
+
+    elif request.method == "POST":
+        nome_nova_conta = request.POST.get("nome_conta")
+
+        nova_conta = ContaFinanceira(
+            usuario=request.user,
+            nome=nome_nova_conta
+        )
+        nova_conta.save()
+
+        return HttpResponseRedirect(
+            reverse("financas:contas_por_usuario")
+        )
+
+
+def nova_transacao(request):
+
+    contas_do_usuario = ContaFinanceira.objects.filter(
+        usuario=request.user
+    )
+
+    if request.method == "GET":
+
+        context = {
+            "contas_do_usuario": contas_do_usuario
+        }
+
+        return render(
+            request,
+            "financas/nova_transacao.html",
+            context
+        )
+
+    elif request.method == "POST":
+
+        conta_debitada_id = int(request.POST.get("conta_debitada_id"))
+        conta_creditada_id = int(request.POST.get("conta_creditada_id"))
+
+        if conta_debitada_id == conta_creditada_id:
+            context = {
+                "erro": "Você não pode definir a mesma conta como débito e crédito",
+                "contas_do_usuario": contas_do_usuario
+            }
+
+            return render(request, "financas/nova_transacao.html", context)
+
+        valor_transacao = request.POST.get("valor_transacao")
+        valor_transacao = valor_transacao.replace(",", ".")
+        valor_transacao = float(valor_transacao)
+
+        conta_debitada = ContaFinanceira.objects.get(pk=conta_debitada_id)
+        conta_creditada = ContaFinanceira.objects.get(pk=conta_creditada_id)
+
+        conta_debitada.saldo -= valor_transacao
+        conta_creditada.saldo += valor_transacao
+
+        transacao = Transacao(
+            conta_debito=conta_debitada,
+            conta_credito=conta_creditada,
+            valor=valor_transacao
+        )
+
+        transacao.save()
+        conta_debitada.save()
+        conta_creditada.save()
+
+        return HttpResponseRedirect(
+            reverse("financas:transacoes_por_usuario")
+        )
+
+
+def detalhe_conta(request, conta_id):
+
+    conta = ContaFinanceira.objects.get(pk=conta_id)
+    transacoes = Transacao.objects.filter(
+        Q(conta_debito=conta) | Q(conta_credito=conta)
+    )
+
+    context = {
+        "conta": conta,
+        "transacoes": transacoes
+    }
+
+    return render(
+        request,
+        "financas/detalhe_conta.html",
+        context
+    )
