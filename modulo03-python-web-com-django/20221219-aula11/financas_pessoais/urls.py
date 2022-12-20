9f303e0f484d694e73a61d249ce6801aad7b4a5f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-12-19 21:25:00 -0300

    Arquivos aula 10 python web

--- modulo03-python-web-com-django/20221219-aula11/financas_pessoais/urls.py
+++ modulo03-python-web-com-django/20221219-aula11/financas_pessoais/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('financas/', include('financas.urls')),
-    path('contas/', include('django.contrib.auth.urls'))
+    path('contas/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/contas/login/'))
 ]
